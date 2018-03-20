adee23576b81aff2918903269558ae9a18e38950
Stefan Iarca <stefan@Stefans-MacBook-Pro.local>
2018-03-20 18:56:32 +0200

    changed setup

--- setup.py
+++ setup.py
@@ -1,14 +1,6 @@
 import sys
 
-# Make sure we are running python3.5+
-if 10 * sys.version_info[0]  + sys.version_info[1] < 35:
-    sys.exit("Sorry, only Python 3.5+ is supported.")
-
 from setuptools import setup
-
-def readme():
-    with open('README.rst') as f:
-        return f.read()
 
 setup(
       name             =   'med2image',
@@ -22,7 +14,5 @@ setup(
       install_requires =   ['nibabel', 'pydicom', 'numpy', 'matplotlib', 'pillow'],
       #test_suite       =   'nose.collector',
       #tests_require    =   ['nose'],
-      scripts          =   ['bin/med2image'],
       license          =   'MIT',
-      zip_safe         =   False
 )
